7b67458479ad9c80086fcde0bf36b42ac56d5e8f
Chiến Uông <chienu74@gmail.com>
2023-10-25 23:34:32 +0700

    th9,10

---
+++ Startup/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using startup.Models;
+
+namespace startup.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PostController : Controller
+    {
+        private readonly DataContext _context;
+        public PostController(DataContext context) 
+        {
+            _context = context; 
+        }
+
+        public IActionResult Create()
+        {
+            var mnList = (from m in _context.Menus
+                          select new SelectListItem()
+                          {
+                              Text = m.MenuName,
+                              Value = m.MenuID.ToString()
+                          }).ToList();
+            mnList.Insert(0, new SelectListItem()
+            {
+                Text= "----Select----",
+                Value = string.Empty
+            });
+            ViewBag.mnList = mnList;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Create");
+        }
+    }
+}
