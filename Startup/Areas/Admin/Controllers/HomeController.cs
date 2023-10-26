69c4098ca99c89ca843dd771d8a120ee67f4512d
Chiến Uông <chienu74@gmail.com>
2023-10-26 14:58:37 +0700

    th11

--- Startup/Areas/Admin/Controllers/HomeController.cs
+++ Startup/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using startup.Utilities;
 
 namespace startup.Areas.Admin.Controllers
 {
@@ -7,7 +8,22 @@ namespace startup.Areas.Admin.Controllers
     {
         public IActionResult Index()
         {
+            if( !Functions.IsLogin())
+                return RedirectToAction("Index", "Login");
             return View();
         }
+        public IActionResult Logout()
+        {
+            Functions._UserID = 0;
+            Functions._UserName = string.Empty;
+            Functions._Email = string.Empty;
+            Functions._Message = string.Empty;
+            Functions._MessageEmail = string.Empty;
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
+
+
 }
