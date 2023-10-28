8cbbd1cbaa7d42b32ad512085a425ae4c5905411
chienu74 <chienu74@gmail.com>
2023-10-28 12:33:46 +0700

    xong post index delete, edit dang dở

--- Startup/Areas/Admin/Controllers/MenuController.cs
+++ Startup/Areas/Admin/Controllers/MenuController.cs
@@ -17,31 +17,6 @@ namespace startup.Areas.Admin.Controllers
             var mnList = _context.Menus.OrderBy(m=>m.MenuID).ToList();
             return View(mnList);
         }
-        public IActionResult Delete(int? id) 
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            var mn = _context.Menus.Find(id);
-            if (mn == null)
-            {
-                return NotFound();
-            }
-            return View(mn);
-        }
-        [HttpPost]
-        public IActionResult Delete(int id)
-        {
-            var deleMenu = _context.Menus.Find(id);
-            if (deleMenu == null)
-            {
-                return NotFound();
-            }
-            _context.Menus.Remove(deleMenu);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
         public IActionResult Create()
         {
             var mnList = (from m in _context.Menus
@@ -107,6 +82,32 @@ namespace startup.Areas.Admin.Controllers
                 return RedirectToAction("Index");
             }
             return View(mn);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var mn = _context.Menus.Find(id);
+            if (mn == null)
+            {
+                return NotFound();
+            }
+            return View(mn);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var deleMenu = _context.Menus.Find(id);
+            if (deleMenu == null)
+            {
+                return NotFound();
+            }
+            _context.Menus.Remove(deleMenu);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- Startup/Areas/Admin/Controllers/PostController.cs
+++ Startup/Areas/Admin/Controllers/PostController.cs
@@ -13,6 +13,12 @@ namespace startup.Areas.Admin.Controllers
             _context = context; 
         }
 
+        public IActionResult Index()
+        {
+            var mnList = _context.Posts.OrderBy(m => m.PostID).ToList();
+            return View(mnList);
+        }
+
         public IActionResult Create()
         {
             var mnList = (from m in _context.Menus
@@ -40,6 +46,70 @@ namespace startup.Areas.Admin.Controllers
                 _context.SaveChanges();
             }
             return RedirectToAction("Create");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var po = _context.Posts.Find(id);
+            if (po == null)
+            {
+                return NotFound();
+            }
+            var polist = (from m in _context.Posts
+                          select new SelectListItem()
+                          {
+                              Text = m.Title,
+                              Value = m.PostID.ToString(),
+                          }).ToList();
+            polist.Insert(0, new SelectListItem()
+            {
+                Text = "----Select----",
+                Value = string.Empty
+            });
+            ViewBag.poList = polist;
+            return View(po);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Post po)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Posts.Update(po);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(po);
+        }
+
+        public IActionResult Delete(long? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var bl = _context.Posts.Find(id);
+            if (bl == null)
+            {
+                return NotFound();
+            }
+            return View(bl);
+        }
+        [HttpPost]
+        public IActionResult Delete(long id)
+        {
+            var delePost = _context.Posts.Find(id);
+            if (delePost == null)
+            {
+                return NotFound();
+            }
+            _context.Posts.Remove(delePost);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
